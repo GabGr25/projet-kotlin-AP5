2cd52ae756e0ed30c545a95862da0908aca86690
Gabriel Gressier <gabfreestyle@gmail.com>
2024-10-08 17:00:56 +0200

    Album Screen

--- app/src/main/java/com/example/projet_kotlin_ap5/components/VerticalCaroussel.kt
+++ app/src/main/java/com/example/projet_kotlin_ap5/components/VerticalCaroussel.kt
@@ -1,8 +1,35 @@
 package com.example.projet_kotlin_ap5.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun VerticalCaroussel(firstColumn: List<String>, secondColumn: List<String>){
+fun VerticalCarousel(items: List<String>) {
 
+    val pairedItems = items.chunked(2)
+
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth().padding(top = 25.dp, bottom = 25.dp),
+
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        items(pairedItems) { pair ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                for (item in pair) {
+                    ClickableImage(item, 175.dp)
+                }
+
+                if (pair.size == 1) {
+                    Spacer(modifier = Modifier.width(175.dp))
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/projet_kotlin_ap5/pages/Album.kt
+++ app/src/main/java/com/example/projet_kotlin_ap5/pages/Album.kt
@@ -2,16 +2,13 @@ package com.example.projet_kotlin_ap5.pages
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.projet_kotlin_ap5.components.Caroussel
-import com.example.projet_kotlin_ap5.components.SquareView
 import com.example.projet_kotlin_ap5.components.TitleText
+import com.example.projet_kotlin_ap5.components.VerticalCarousel
 
 @Composable
 fun Album(modifier: Modifier){
@@ -22,9 +19,19 @@ fun Album(modifier: Modifier){
         verticalArrangement = Arrangement.Top
     ) {
         TitleText("Albums préférés", 0.55f)
-        SquareView(
-            listOf("img1", "img2", "img5", "img7"),
-            listOf("img3", "img4", "img6", "img8")
+        VerticalCarousel(
+            listOf(
+                "img1",
+                "img2",
+                "img5",
+                "img7",
+                "img3",
+                "img4",
+                "img6",
+                "img8",
+                "img9",
+                "img10"
+            )
         )
     }
 }
