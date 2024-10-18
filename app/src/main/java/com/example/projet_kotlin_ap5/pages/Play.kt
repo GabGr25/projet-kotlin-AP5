741b939ec1df96366e2a174a2c5d61b337dcaa7e
Zackaliok <zackaliok.contact@gmail.com>
2024-10-19 00:13:54 +0200

    Play/Pause logic

--- app/src/main/java/com/example/projet_kotlin_ap5/pages/Play.kt
+++ app/src/main/java/com/example/projet_kotlin_ap5/pages/Play.kt
@@ -18,31 +18,72 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.projet_kotlin_ap5.components.TitleText
 
-fun getImageResourceId(context: Context, nameImage: String): Int? {
-    val resourceId = context.resources.getIdentifier(nameImage, "drawable", context.packageName)
-    return if (resourceId != 0) resourceId else null
-}
-
 @Composable
 fun Play(navController: NavController) {
     val context = LocalContext.current
     // Obtenir l'identifiant de l'image dynamiquement à partir du nom
-    val imageId = com.example.projet_kotlin_ap5.components.getImageResourceId(context, "img1")
+    val play_icon = com.example.projet_kotlin_ap5.components.getImageResourceId(context, "play_icon")
+    val pause_icon = com.example.projet_kotlin_ap5.components.getImageResourceId(context, "pause_icon")
+    val next_icon = com.example.projet_kotlin_ap5.components.getImageResourceId(context, "skip_forward_icon")
+    val previous_icon = com.example.projet_kotlin_ap5.components.getImageResourceId(context, "skip_previous_icon")
+
+    // Media player
+    val mediaPlayer = loadSong(context)
 
     Column(
         verticalArrangement = Arrangement.Top
     ) {
-        TitleText("Écoutés récemment", 0.66f)
-        imageId?.let {
+        TitleText("DEV", 0.66f)
+        play_icon?.let {
             Image(
                 painter = painterResource(id = it),
                 contentDescription = "Clickable Image",
                 modifier = androidx.compose.ui.Modifier
-                    .size(140.dp)
+                    .size(50.dp)
                     .clip(RoundedCornerShape(5.dp))
                     .clickable {
                         Log.d("dev", "Playing")
-                        playKaaris(context)
+                        if (mediaPlayer != null) togglePlay(mediaPlayer)
+                    },
+                contentScale = ContentScale.Crop // Ajustement de l'image
+            )
+        }
+        pause_icon?.let {
+            Image(
+                painter = painterResource(id = it),
+                contentDescription = "Clickable Image",
+                modifier = androidx.compose.ui.Modifier
+                    .size(50.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .clickable {
+                        Log.d("dev", "Pausing")
+                        if (mediaPlayer != null) togglePlay(mediaPlayer)
+                    },
+                contentScale = ContentScale.Crop // Ajustement de l'image
+            )
+        }
+        next_icon?.let {
+            Image(
+                painter = painterResource(id = it),
+                contentDescription = "Clickable Image",
+                modifier = androidx.compose.ui.Modifier
+                    .size(50.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .clickable {
+                        next()
+                    },
+                contentScale = ContentScale.Crop // Ajustement de l'image
+            )
+        }
+        previous_icon?.let {
+            Image(
+                painter = painterResource(id = it),
+                contentDescription = "Clickable Image",
+                modifier = androidx.compose.ui.Modifier
+                    .size(50.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .clickable {
+                        previous()
                     },
                 contentScale = ContentScale.Crop // Ajustement de l'image
             )
@@ -50,14 +91,43 @@ fun Play(navController: NavController) {
     }
 }
 
-fun playKaaris(context: Context) {
+fun loadSong(context: Context): MediaPlayer? {
     // Identifier la ressource dans le dossier "audio" avec le nom du fichier, sans extension
     val resourceId = context.resources.getIdentifier("or_noir", "raw", context.packageName)
 
     if (resourceId != 0) {
         val mediaPlayer = MediaPlayer.create(context, resourceId)
-        mediaPlayer?.start() // Démarre la lecture
+        return mediaPlayer
     } else {
         Log.e("dev", "La ressource audio est introuvable")
+        return null
     }
+}
+
+fun togglePlay(mediaPlayer: MediaPlayer) {
+    if (mediaPlayer.isPlaying) {
+        pause(mediaPlayer)
+    } else {
+        play(mediaPlayer)
+    }
+}
+
+fun play(mediaPlayer: MediaPlayer) {
+    if (!mediaPlayer.isPlaying) {
+        mediaPlayer.start()
+    }
+}
+
+fun pause(mediaPlayer: MediaPlayer) {
+    if (mediaPlayer.isPlaying) {
+        mediaPlayer.pause()
+    }
+}
+
+fun next() {
+    Log.d("dev", "Next")
+}
+
+fun previous() {
+    Log.d("dev", "Previous")
 }
