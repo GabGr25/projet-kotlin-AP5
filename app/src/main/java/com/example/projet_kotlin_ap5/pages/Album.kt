b373760f9323f82ffff7ea8e4ed793290bec106a
Zackaliok <57079892+Zackaliok@users.noreply.github.com>
2024-10-20 22:50:57 +0200

    Album listing + Song infos (#6)
    * Album listing + redirection to audioPlayer when clicked (loading album too)
    
    * Song Info in PlayerAudio.kt

--- app/src/main/java/com/example/projet_kotlin_ap5/pages/Album.kt
+++ app/src/main/java/com/example/projet_kotlin_ap5/pages/Album.kt
@@ -1,5 +1,6 @@
 package com.example.projet_kotlin_ap5.pages
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,31 +9,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.projet_kotlin_ap5.components.ClickableImage
 import com.example.projet_kotlin_ap5.components.TitleText
 import com.example.projet_kotlin_ap5.components.VerticalCarousel
+import com.example.projet_kotlin_ap5.models.SongViewModel
+import com.example.projet_kotlin_ap5.services.AudioPlayerService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 @Composable
-fun Album(navController: NavController, modifier: Modifier = Modifier){
+fun Album(navController: NavController, songViewModel: SongViewModel, audioPlayerService: AudioPlayerService, modifier: Modifier = Modifier){
+
+    val albums: MutableList<String> = emptyList<String>().toMutableList()
+    runBlocking {
+        withContext(Dispatchers.IO) {
+            val rawAlbums = songViewModel.getAlbums()
+            for (album in rawAlbums) {
+                albums += album.lowercase().replace(" ", "_")
+            }
+            Log.d("dev", albums.toString())
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(top = 10.dp),
         verticalArrangement = Arrangement.Top
     ) {
-        TitleText("Albums préférés", 0.55f)
-        VerticalCarousel(navController,
-            listOf(
-                "img1",
-                "img2",
-                "img5",
-                "img7",
-                "img3",
-                "img4",
-                "img6",
-                "img8",
-                "img9",
-                "img10"
-            )
-        )
+        TitleText("Albums", 0.55f)
+        if (albums.isNotEmpty()) {
+            VerticalCarousel(navController, albums, clickableImageCallback = { album ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    audioPlayerService.loadAlbum(album)
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/example/projet_kotlin_ap5/services/AudioPlayerService.kt
+++ app/src/main/java/com/example/projet_kotlin_ap5/services/AudioPlayerService.kt
@@ -5,93 +5,99 @@ import android.os.Environment
 import android.util.Log
 import com.example.projet_kotlin_ap5.entities.SongEntity
 import com.example.projet_kotlin_ap5.models.SongViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import java.io.File
 
 class AudioPlayerService(private val songViewModel: SongViewModel) {
     val mediaPlayer = MediaPlayer()
     val musicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
 
-    var currentSong: SongEntity? = null
+    private val _currentSong = MutableStateFlow<SongEntity?>(null)
+    val currentSongFlow: StateFlow<SongEntity?> = _currentSong.asStateFlow() // Exposer StateFlow en lecture seule
     var currentPlaylist: List<SongEntity>? = null
 
+    // Charger un album entier
     suspend fun loadAlbum(album: String) {
         val loadedAlbum = songViewModel.getSongsByAlbum(album)
         currentPlaylist = loadedAlbum
-        currentSong = loadedAlbum[0]
-        Log.d("dev", "Album loaded : $album avec ${loadedAlbum.size} chansons. Chanson en cours : ${currentSong!!.title}")
+        _currentSong.value = loadedAlbum[0]
+        loadSong(_currentSong.value!!)
+        Log.d("dev", "Album loaded: $album with ${loadedAlbum.size} songs. Current song: ${_currentSong.value!!.title}")
     }
 
+    // Sauter à la chanson suivante
     fun skipToNextSong() {
         stop()
-        if (currentPlaylist != null && currentSong != null) {
-            val currentIndex = currentPlaylist!!.indexOf(currentSong)
+        if (currentPlaylist != null && _currentSong.value != null) {
+            val currentIndex = currentPlaylist!!.indexOf(_currentSong.value)
 
             if (currentIndex < currentPlaylist!!.size - 1) {
-                currentSong = currentPlaylist!![currentIndex + 1]
+                _currentSong.value = currentPlaylist!![currentIndex + 1]
             } else {
-                // If we are at the end of the playlist, go to the first song
-                currentSong = currentPlaylist!![0]
+                _currentSong.value = currentPlaylist!![0] // Revenir à la première chanson si à la fin de la liste
             }
 
-            loadSong(currentSong!!)
+            loadSong(_currentSong.value!!)
             play()
         }
     }
 
+    // Revenir à la chanson précédente
     fun skipToPreviousSong() {
         stop()
-        if (currentPlaylist != null && currentSong != null) {
-            val currentIndex = currentPlaylist!!.indexOf(currentSong)
+        if (currentPlaylist != null && _currentSong.value != null) {
+            val currentIndex = currentPlaylist!!.indexOf(_currentSong.value)
             if (currentIndex > 0) {
-                currentSong = currentPlaylist!![currentIndex - 1]
+                _currentSong.value = currentPlaylist!![currentIndex - 1]
             } else {
-                // If we are at the beginning of the playlist, go to the last song
-                currentSong = currentPlaylist!![currentPlaylist!!.size - 1]
+                _currentSong.value = currentPlaylist!![currentPlaylist!!.size - 1] // Aller à la dernière chanson si au début de la liste
             }
 
-            loadSong(currentSong!!)
+            loadSong(_currentSong.value!!)
             play()
         }
     }
 
-
-        // Media player section
-        fun loadSong(songEntity: SongEntity) {
+    // Charger une chanson spécifique
+    fun loadSong(songEntity: SongEntity) {
+        stop()
+        try {
             mediaPlayer.reset()
-            try {
-                val filePath = File(musicDir, songEntity.pathName+"/"+songEntity.fileName).absolutePath
-                mediaPlayer.setDataSource(filePath)
-                mediaPlayer.prepare()
-            } catch (e: Exception) {
-                Log.e("dev", "Erreur lors du chargement de la chanson", e)
-            }
+            val filePath = File(musicDir, songEntity.pathName + "/" + songEntity.fileName).absolutePath
+            mediaPlayer.setDataSource(filePath)
+            mediaPlayer.prepare()
+            _currentSong.value = songEntity // Mettre à jour la chanson courante
+        } catch (e: Exception) {
+            Log.e("dev", "Error loading song", e)
         }
-
-        fun togglePlay() {
-            if (mediaPlayer.isPlaying) {
-                pause()
-            } else {
-                play()
-            }
-        }
-
-        private fun play() {
-            if (!mediaPlayer.isPlaying) {
-                mediaPlayer.start()
-            }
-        }
-
-        private fun pause() {
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.pause()
-            }
-        }
-
-        private fun stop() {
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.stop()
-            }
-        }
-
     }
 
+    // Lecture/Pause
+    fun togglePlay() {
+        if (mediaPlayer.isPlaying) {
+            pause()
+        } else {
+            play()
+        }
+    }
+
+    private fun play() {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+        }
+    }
+
+    private fun pause() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+        }
+    }
+
+    private fun stop() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+    }
+}
